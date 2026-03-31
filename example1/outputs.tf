29d0e1f499bda0d4a4bf8c467785f20e4acce139
naveenbasra <40802649+naveenbasra@users.noreply.github.com>
2026-03-31 22:02:10 +1100

    Update outputs.tf

--- example1/outputs.tf
+++ example1/outputs.tf
@@ -1,6 +1,6 @@
 output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
+  value       = aws_instance.example.private_dns
 }
 
 output "instance_security_group_ids" {
